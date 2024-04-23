ab14beb722535124ac9f1a4207530c0a6d58aa18
zufar <ramadhany611@gmail.com>
2024-04-23 18:32:22 -0400

    js to make continue hide/disappear properly

---
+++ first run boys/app.js
@@ -0,0 +1,5 @@
+function attacked(){
+    document.getElementById("theDarkness").innerHTML="<h1>Hmph! I see...</h1>";
+    document.getElementById("continue").style.visibility = 'visible';
+    document.getElementById("breakthrough").innerHTML="Nice!";
+}
