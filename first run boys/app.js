a016305d020e2ffd3f3dd834147ce54b95846fb3
zufar <ramadhany611@gmail.com>
2024-04-23 18:40:58 -0400

    remove instead of hide js

--- first run boys/app.js
+++ first run boys/app.js
@@ -2,4 +2,6 @@ function attacked(){
     document.getElementById("theDarkness").innerHTML="<h1>Hmph! I see...</h1>";
     document.getElementById("continue").style.visibility = 'visible';
     document.getElementById("breakthrough").innerHTML="Nice!";
+    document.getElementById("toolkit").remove();
+    document.getElementById("toolkit header").remove();
 }
